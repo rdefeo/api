a371f14eff3ffbfddebea74719c26b9e655751d5
Rob De Feo <robertodefeo@hotmail.com>
2015-03-23 13:21:27 +0100

    add status of context service close #4

--- api/handlers/status.py
+++ api/handlers/status.py
@@ -3,7 +3,7 @@ import tornado
 from tornado.httpclient import AsyncHTTPClient
 from tornado.web import RequestHandler, asynchronous
 from tornado.escape import json_encode
-from api.settings import DETECT_URL, SUGGEST_URL
+from api.settings import DETECT_URL, SUGGEST_URL, CONTEXT_URL
 
 
 class Status(RequestHandler):
@@ -17,7 +17,7 @@ class Status(RequestHandler):
     @gen.engine
     def get(self):
         http_client = AsyncHTTPClient()
-        detect_response, suggest_response = yield [
+        detect_response, suggest_response, context_response = yield [
             gen.Task(
                 http_client.fetch,
                 "%s/status" % DETECT_URL
@@ -25,11 +25,16 @@ class Status(RequestHandler):
             gen.Task(
                 http_client.fetch,
                 "%s/status" % SUGGEST_URL
+            ),
+            gen.Task(
+                http_client.fetch,
+                "%s/status" % CONTEXT_URL
             )
         ]
         services = [
             self.check_service_status(detect_response, "detect"),
-            self.check_service_status(suggest_response, "suggest")
+            self.check_service_status(suggest_response, "suggest"),
+            self.check_service_status(context_response, "context")
         ]
         self.set_header('Content-Type', 'application/json')
         self.set_status(200)
@@ -51,5 +56,3 @@ class Status(RequestHandler):
                 "status": response.reason,
                 "error": str(response.error)
             }
-        pass
-        # if response.
